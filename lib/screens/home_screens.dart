ee180b356b5359a32e83100bd5eb525d07113510
João Gabriel <jgmoura.dev@gmail.com>
2022-11-08 22:37:35 -0300

    tela na web funcionando

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,12 +1,31 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreens extends StatelessWidget {
-  const HomeScreens({super.key});
+class HomeScreens extends StatefulWidget {
+  @override
+  State<HomeScreens> createState() => _HomeScreensState();
+}
+
+class _HomeScreensState extends State<HomeScreens> {
+  //const HomeScreens({super.key});
+
+  Future<User?> _login(String username, String password) async {
+    try {
+      final response = await Dio().get(
+          'https://tecbleus-app-api-dev.herokuapp.com/login/v1/app?username=$username&password=$password');
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  TextEditingController userController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.grey[200],
       body: Container(
         padding: const EdgeInsets.all(15),
         alignment: Alignment.center,
@@ -15,96 +34,114 @@ class HomeScreens extends StatelessWidget {
             elevation: 8,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'images/logo.png',
-                  width: 150,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 50, right: 50, bottom: 30),
-                  child: Form(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                                label: Text(
-                              'Usuário',
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            )),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          TextFormField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                suffixIcon: IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(Icons.remove_red_eye)),
-                                label: Text(
-                                  'Senha',
-                                  style: TextStyle(color: Colors.grey),
-                                )),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Container(
-                            height: 50,
-                            decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                    colors: [Colors.green, Colors.blue])),
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
+            child: SizedBox(
+              height: 600,
+              width: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'images/logo.png',
+                    width: 150,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 50, right: 50, bottom: 30),
+                    child: Form(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            TextFormField(
+                              controller: userController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                  label: Text(
+                                'Usuário',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              )),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              controller: passwordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.remove_red_eye)),
+                                  label: Text(
+                                    'Senha',
+                                    style: TextStyle(color: Colors.grey),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  gradient: LinearGradient(
+                                      colors: [Colors.green, Colors.blue])),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
                                   primary: Colors.transparent,
                                   shadowColor: Colors.transparent,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10))),
-                              child: Text(
-                                'LOGIN',
-                                style: TextStyle(fontSize: 15),
+                                ),
+                                child: Text(
+                                  'LOGIN',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onPressed: () {
+                                  _login(userController.text,
+                                      passwordController.text);
+                                },
                               ),
-                              onPressed: () {},
                             ),
-                          ),
-                          SizedBox(
-                            height: 16,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Don’t have an account?",
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                              TextButton(
-                                  onPressed: () {},
-                                  child: Text(
-                                    "Sign Up",
-                                    style: TextStyle(
-                                      color: Colors.green,
-                                    ),
-                                  ))
-                            ],
-                          )
-                        ]),
-                  ),
-                )
-              ],
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Não possui uma conta?",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      "Cadastre-se",
+                                      style: TextStyle(
+                                          color: Colors.green,
+                                          fontWeight: FontWeight.bold),
+                                    ))
+                              ],
+                            )
+                          ]),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+}
+
+class User {
+  final String username;
+  final String password;
+
+  User(this.username, this.password);
 }
